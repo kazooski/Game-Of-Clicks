13752eb97288c5b6264c2623a08527fe6fcb628e
noisette <kasusky.music@gmail.com>
2020-09-19 23:08:40 -0700

    images included; navbar and header components created

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './component/Navbar';
+import Header from './component/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state={
+    score: 0,
+    topScore: 0
+  }
+  
+  render () {
+    return(
+      <div className="container">
+        <Navbar
+          score={this.state.score}
+          topScore={this.state.topScore}
+        />
+        <Header />
+
+      </div>
+
+    )
+    
+  }
 }
 
 export default App;
